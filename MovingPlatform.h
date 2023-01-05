97586273b55778ebb5675fde715f7916804797f8
Niddish <121927267+Niddish@users.noreply.github.com>
2023-01-05 16:03:49 -0500

    Update MovingPlatform.h

--- MovingPlatform.h
+++ MovingPlatform.h
@@ -22,5 +22,20 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(EditAnywhere)
+	int32 MyInt = 99;
+
+	UPROPERTY(EditAnywhere)
+	int32 InputA = 0;
+
+	UPROPERTY(EditAnywhere)
+	int32 InputB = 0;
+
+	UPROPERTY(EditAnywhere)
+	int32 APlusB = 0;
+
+	UPROPERTY(EditAnywhere)
+	float MyFloat = 6.99;
 
 };
